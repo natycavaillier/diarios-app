bfefdc40b1028f810f8d670f2195b380c531cdea
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-05-25 13:43:52 -0300

    Install: Setup Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'diario-viagens-c4d6b',
+    appId: '1:45382860833:web:fc9f78feacd3d345df6360',
+    storageBucket: 'diario-viagens-c4d6b.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBUiqxi-Yx7XVeCf6QKM5U6G3SnFnv6Dyw',
+    authDomain: 'diario-viagens-c4d6b.firebaseapp.com',
+    messagingSenderId: '45382860833',
+  },
   production: true
 };
