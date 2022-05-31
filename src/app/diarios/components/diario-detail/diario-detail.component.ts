a9aacb2e27b03e0bae94c548022c29c91538bb50
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-05-31 13:54:44 -0300

    Adição da página de detalhes do diario

--- src/app/diarios/components/diario-detail/diario-detail.component.ts
+++ src/app/diarios/components/diario-detail/diario-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Diario } from 'src/app/core/models/diario';
+import { DiariosService } from 'src/app/core/services/diarios/diarios.service';
 
 @Component({
   selector: 'app-diario-detail',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiarioDetailComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(
+    //Guarda informações sobre a rota atual
+    private route: ActivatedRoute,
+    private diariosService: DiariosService
+  ) { }
+
+  diario$?: Observable<Diario>;
 
   ngOnInit(): void {
+    this.diario$ = this.diariosService.getDiarioById(
+      this.route.snapshot.params['id']);
   }
 
 }
